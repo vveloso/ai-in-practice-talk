0432c91f7529290675934a1656dd756479f223c8
Vasco Veloso <vveloso@gmail.com>
2023-05-05 10:58:04 +0200

    Shorten Fashion-MNIST sample code.

--- fashion/fashion-build.py
+++ fashion/fashion-build.py
@@ -31,46 +31,14 @@ classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 
 # %%
 
-# Training a simple model
-
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(300, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
-model.compile(optimizer='adam',
-              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
-              metrics=['accuracy'])
-
-history = model.fit(train_images, train_labels, 
-                    epochs=10, 
-                    validation_split=0.1)
-
-test_loss, test_acc = model.evaluate(test_images,  test_labels, 
-    verbose=2)
-
-draw.plot_history(history)
-
-#%% 
-
-model.save('fashion')
-
-# %%
-
-new_train_images = tf.expand_dims(train_images, 3)
-new_test_images = tf.expand_dims(test_images, 3)
-
-# %%
-
 # Training an improved model
+# see "Fashion- MNIST with tf.Keras", Margaret Maynard-Reid, 2018
 
 model = tf.keras.Sequential([
-    tf.keras.layers.Conv2D(64, 2, padding='same',
-        activation='relu'),
-    tf.keras.layers.MaxPool2D(2),
-    tf.keras.layers.Dropout(0.3),
-    tf.keras.layers.Conv2D(32, 2, padding='same', 
-        activation='relu'),
+    tf.keras.layers.Conv2D(64, 2, padding='same', activation='relu'), # spatial convolution over images
+    tf.keras.layers.MaxPool2D(2), # max pooling operation for 2D spatial data (downsampling).
+    tf.keras.layers.Dropout(0.3), # randomly sets input units to 0 with a given frequency at each step during training time
+    tf.keras.layers.Conv2D(32, 2, padding='same', activation='relu'),
     tf.keras.layers.MaxPool2D(2),
     tf.keras.layers.Dropout(0.3),
     tf.keras.layers.Flatten(),
@@ -82,16 +50,15 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(),
               metrics=['accuracy'])
 
-history = model.fit(new_train_images, train_labels, 
+history = model.fit(train_images, train_labels, 
                     epochs=10, 
                     validation_split=0.1)
 
-test_loss, test_acc = model.evaluate(new_test_images,  test_labels, 
-    verbose=2)
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 draw.plot_history(history)
 
 #%%
 
-model.save('fashion-2')
+# model.save('fashion-cnn')
 
